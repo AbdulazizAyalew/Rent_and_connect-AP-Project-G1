0ae7645c202100cf5bdbdf038eb451d51e1f24ba
AbdulazizAya <abdulazizayalewmussa@gmail.com>
2025-05-26 15:54:13 +0300

    Connected and Initialized the database

--- src/db/DatabaseInitializer.java
+++ src/db/DatabaseInitializer.java
@@ -16,12 +16,11 @@ public class DatabaseInitializer {
     try (Connection dbConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/rent_connect", "root", "12345678")) {
         Statement dbStmt = dbConn.createStatement();
 
-        dbStmt.executeUpdate("CREATE TABLE IF NOT EXISTS renters (id INT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(100), email VARCHAR(100),phone_no VARCHAR(15),password VARCHAR(255), city VARCHAR(100))");
-        dbStmt.executeUpdate("CREATE TABLE IF NOT EXISTS lenders (id INT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(100), email VARCHAR(100),password VARCHAR(255),city VARCHAR(100))");
-        dbStmt.executeUpdate("CREATE TABLE IF NOT EXISTS house_listings (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(100), description TEXT,house_type VARCHAR(50),location VARCHAR(255),price DECIMAL(10,2), lender_id INT,FOREIGN KEY (lender_id) REFERENCES lenders(id) ON DELETE CASCADE,availability BOOLEAN)");
-        dbStmt.executeUpdate("CREATE TABLE IF NOT EXISTS messages (id INT PRIMARY KEY AUTO_INCREMENT, sender VARCHAR(50), receiver_id INT, content TEXT,sent_at DATETIME DEFAULT CURRENT_TIMESTAMP)");
+        dbStmt.executeUpdate("CREATE TABLE IF NOT EXISTS renters (renter_id INT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(100), password VARCHAR(255), city VARCHAR(100), registration_day DATETIME DEFAULT CURRENT_TIMESTAMP)");
+        dbStmt.executeUpdate("CREATE TABLE IF NOT EXISTS lenders (lender_id INT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(100), phone_no VARCHAR(15), password VARCHAR(255), joined_date DATETIME DEFAULT CURRENT_TIMESTAMP)");
+        dbStmt.executeUpdate("CREATE TABLE IF NOT EXISTS house_listings (house_id INT AUTO_INCREMENT PRIMARY KEY,lender_id INT, title VARCHAR(150), description TEXT, image_path VARCHAR(255), house_type VARCHAR(50), location VARCHAR(255), price DECIMAL(10,2), availability BOOLEAN, FOREIGN KEY (lender_id) REFERENCES lenders(lender_id) ON DELETE CASCADE, created_at DATETIME DEFAULT CURRENT_TIMESTAMP)");
 
-        System.out.println("✅ Database and tables created successfully!");
+        System.out.println(">> Database and tables created successfully!");
     }
 
 } catch (Exception e) {
