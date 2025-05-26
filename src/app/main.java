76c74a57fa7386344ed02a424f16f0f3adccbdef
AbdulazizAya <abdulazizayalewmussa@gmail.com>
2025-05-26 16:32:41 +0300

    Add the Application starting Code

--- src/app/main.java
+++ src/app/main.java
@@ -3,11 +3,7 @@ import ui.MainMenu;
 
 public class main {
     public static void main(String[] args) {
-        // Show banner or welcome message
-        System.out.println("=======================================");
-        System.out.println("      WELCOME TO RENT & CONNECT        ");
-        System.out.println("=======================================");
-
+        
         // Start the main menu
         new MainMenu().show();
     }
